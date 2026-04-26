3ef5baccbf37362c8024140ddbd7772985cf2bd4
NaufalGavin <gavinadhif00@gmail.com>
2026-04-26 16:16:39 +0700

    upload tugas DPW

---
+++ latihan9.php
@@ -0,0 +1,17 @@
+<?php
+
+function writeMsg($nama) {
+    echo "selamat datang " . $nama . " <br>";
+}
+
+writeMsg("Naufal Gavin");
+
+function tambah(int $angka1, int $angka2) {
+    $a = $angka1 + $angka2;
+    return $a; // mengirim nilai $a ke pemanggil
+}
+
+$hasil = tambah(5, 5);
+echo ($hasil);
+
+?>
